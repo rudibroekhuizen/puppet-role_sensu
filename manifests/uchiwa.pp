29ecb171fc4c76ff32c79234279073fb171b17c3
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-12-30 15:34:16 +0100

    Create uchiwa.pp

---
+++ manifests/uchiwa.pp
@@ -0,0 +1,40 @@
+# == Class: role_sensu::uchiwa.pp
+#
+class role_sensu::uchiwa.pp {
+
+  package { 'uchiwa':
+    ensure => present,
+  }
+
+  file { '/etc/sensu/uchiwa.json':
+    ensure  => present,
+    content => '
+  {
+    "sensu": [
+      {
+        "name": "Site1",
+        "host": "localhost",
+        "port": 4567,
+        "timeout": 5,
+        "user": "admin",
+        "pass": "secret"
+      }
+    ],
+    "uchiwa": {
+      "host": "0.0.0.0",
+      "port": 3000,
+      "user": "uchiwa",
+      "pass": "uchiwa",
+      "interval": 5
+    }
+  }',
+    require => Package['uchiwa'],
+    notify  => Service['uchiwa'],
+  }
+
+  service { 'uchiwa':
+    ensure  => running,
+    require => [ File['/etc/sensu/uchiwa.json'],Package['uchiwa'] ]
+  }
+  
+}
