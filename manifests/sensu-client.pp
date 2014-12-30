56942ca744eb904f84228133d014b9604226a016
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-12-30 11:56:03 +0100

    Create sensu-client.pp

---
+++ manifests/sensu-client.pp
@@ -0,0 +1,13 @@
+# == Class: role_sensu::sensu-client.pp
+#
+class role_sensu::sensu-client.pp {
+
+  class { 'sensu':
+    rabbitmq_password => 'correct-horse-battery-staple',
+    rabbitmq_host     => '192.168.56.10',
+    rabbitmq_vhost    => '/sensu',
+    subscriptions     => 'all',
+    client_address    => $::ipaddress_eth1,
+  }
+  
+}
