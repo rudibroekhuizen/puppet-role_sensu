6628fac4fd078a876ed7f3cfccba045ba5941c6f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-12-30 21:19:08 +0100

    Update rabbitmq.pp

--- manifests/rabbitmq.pp
+++ manifests/rabbitmq.pp
@@ -13,16 +13,19 @@ class role_sensu::rabbitmq.pp {
   exec { "vhost-sensu":
     command => "rabbitmqctl add_vhost /sensu",
     unless  => "rabbitmqctl list_vhosts | grep sensu",
+    require => [Exec['user-sensu'],Exec['vhost-sensu']]
   }
 
   exec { "user-sensu":
     command => "rabbitmqctl add_user sensu $sensu_passwd",
     unless  => "rabbitmqctl list_users | grep sensu",
+    require => [Exec['user-sensu'],Exec['vhost-sensu']]
   }
 
   exec { "tags-sensu":
     command => "rabbitmqctl set_user_tags sensu administrator",
     unless  => "rabbitmqctl list_users | grep administrator",
+    require => [Exec['user-sensu'],Exec['vhost-sensu']]
   }
 
   exec { "perm-sensu":
