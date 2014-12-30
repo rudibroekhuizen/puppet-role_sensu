1feb13322e0496a065950c228e3537ac0baf33de
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-12-30 15:47:36 +0100

    Update rabbitmq.pp

--- manifests/rabbitmq.pp
+++ manifests/rabbitmq.pp
@@ -2,8 +2,33 @@
 #
 class role_sensu::rabbitmq.pp {
 
+  Exec { path => [ "/opt/rabbitmq/sbin", "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+  
   class {'::rabbitmq':
     delete_guest_user => true,
   }
-  
+ 
+  $sensu_passwd = hiera('rq::sensu')  
+
+  exec { "vhost-sensu":
+    command => "rabbitmqctl add_vhost /sensu",
+    unless  => "rabbitmqctl list_vhosts | grep sensu",
+  }
+
+  exec { "user-sensu":
+    command => "rabbitmqctl add_user sensu $sensu_passwd",
+    unless  => "rabbitmqctl list_users | grep sensu",
+  }
+
+  #exec { "tags-sensu":
+  #  command => "rabbitmqctl set_user_tags sensu administrator",
+  #  unless  => "rabbitmqctl list_users | grep sensu",
+  #}
+
+  exec { "perm-sensu":
+    command => 'rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"',
+    unless  => "rabbitmqctl list_permissions -p /sensu | grep -E 'sensu.*\.\*.*\.\*.*\.\*'",
+    require => [Exec['user-sensu'],Exec['vhost-sensu']]
+  }    
+
 }
