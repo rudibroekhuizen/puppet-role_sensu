9b85024d38263236439c1563adeb32721f768b28
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-02 23:22:47 +0100

    Update dashboard.pp

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -1,13 +1,12 @@
 # == Class: role_sensu::dashboard
 #
 class role_sensu::dashboard (
-  $test = role_sensu::server::api_user,
   $uchiwa_api_config = [ { host     => 'localhost',
                            ssl      => false,
                            insecure => false,
                            port     => 4567,
-                           user     => $test,
-                           pass     => 'secret',
+                           user     => 'admin',  #api_user
+                           pass     => 'secret', #api_pass
                            timeout  => 5
                           } ],
   $uchiwa_user = 'admin',
