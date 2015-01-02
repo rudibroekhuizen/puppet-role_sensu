22312816868f7b21ffa10f9e3519ddddc71aa5b7
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-02 12:38:28 +0100

    Update rabbitmq.pp

--- manifests/rabbitmq.pp
+++ manifests/rabbitmq.pp
@@ -34,7 +34,7 @@ class role_sensu::rabbitmq (
 
   exec { "perm-sensu":
     command => 'rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"',
-    unless  => "rabbitmqctl list_permissions -p /sensu | grep -E 'sensu.*\.\*.*\.\*.*\.\*'",
+    unless  => 'rabbitmqctl list_permissions -p /sensu | grep -E 'sensu.*\.\*.*\.\*.*\.\*'',
     require => [Class['::rabbitmq'], Exec['vhost-sensu'], Exec['user-sensu']]
   }    
 
