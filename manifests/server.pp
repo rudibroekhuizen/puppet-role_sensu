197b9633016f9c76576241c5e214c64eafba4a4d
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-02 22:50:01 +0100

    Update server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,19 +1,9 @@
 # == Class: role_sensu::server
 #
 class role_sensu::server (
-  $rabbitmq_password = 'secret',
-  $api_user          = 'admin',
-  $api_password      = 'secret',
-  $uchiwa_api_config = [ { host     => 'localhost',
-                           ssl      => false,
-                           insecure => false,
-                           port     => 4567,
-                           user     => $api_user,
-                           pass     => $api_pass,
-                           timeout  => 5
-                          } ],
-  $uchiwa_user = 'admin',
-  $uchiwa_pass = 'secret',
+  $rabbitmq_password,
+  $api_user,
+  $api_password,
   ) {
 
   class { 'redis': } ->
@@ -29,15 +19,8 @@ class role_sensu::server (
     rabbitmq_vhost    => '/sensu',
     api               => true,
     api_user          => $api_user,
-    api_password      => $api_pass,
+    api_password      => $api_password,
     #client_address    => $::ipaddress_eth1
-  } ->
-
-  # Install uchiwa, username and password for uchiwa webinterface, port 3000.
-  class { 'uchiwa':
-    sensu_api_endpoints => $uchiwa_api_config,
-    user                => $uchiwa_user,
-    pass                => $uchiwa_pass,
   }
 
   # Example handler
