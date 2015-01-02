20e26f937687623667f0741abc7322773b45a045
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-02 23:01:48 +0100

    Update dashboard.pp

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -5,7 +5,7 @@ class role_sensu::dashboard (
                            ssl      => false,
                            insecure => false,
                            port     => 4567,
-                           user     => 'admin',
+                           user     => $server::api_user,
                            pass     => 'secret',
                            timeout  => 5
                           } ],
