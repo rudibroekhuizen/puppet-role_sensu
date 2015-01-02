2bd0cd6adcd07a7d124b2211fa91314308da9c4f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-02 22:35:12 +0100

    Update install_uchiwa.pp

--- manifests/install_uchiwa.pp
+++ manifests/install_uchiwa.pp
@@ -1,24 +1,24 @@
 # == Class: role_sensu::install_uchiwa
 #
 class role_sensu::install_uchiwa (
-  $uchiwa_api_config = [{
-                          host     => 'localhost',
-                          ssl      => false,
-                          insecure => true,
-                          port     => 4567,
-                          user     => 'admin',
-                          pass     => 'secret',
-                          path     => '/sensu',
-                          timeout  => 5
-                        }]
+  $uchiwa_api_config = [ { host     => 'localhost',
+                           ssl      => false,
+                           insecure => false,
+                           port     => 4567,
+                           user     => 'admin',
+                           pass     => 'secret',
+                           timeout  => 5
+                          } ],
+  $uchiwa_user = 'admin',
+  $uchiwa_pass = 'admin',
 
   ) {
 
-  # username and password for webinterface, port 3000
+  # username and password for uchiwa webinterface, port 3000
   class { 'uchiwa':
     sensu_api_endpoints => $uchiwa_api_config,
-    user     => 'admin',
-    pass     => 'secret',
+    user                => $uchiwa_user,
+    pass                => $uchiwa_pass,
   }
 
 }
