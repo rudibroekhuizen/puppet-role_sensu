105f92d213c1c02f821cd0f068ca2a45947d6c91
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-02 14:57:48 +0100

    Update server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -33,7 +33,7 @@ class role_sensu::server (
   }
   
   # check-procs.rb will run on clients, where subscriptions is 'sensu-test-rudi'
-  # check-procs.rb file must be available on sensu-server, not on sensu-client
+  # check-procs.rb file must be available on sensu-client
   sensu::check { "check-procs_cron":
     command     => '/opt/sensu-community-plugins/plugins/processes/check-procs.rb -p cron -C 1 ',
     handlers    => 'default',
