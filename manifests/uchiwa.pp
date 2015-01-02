55e2ff8fdff78a15df667ecd7f9542971120c22f
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-02 15:29:22 +0100

    Update uchiwa.pp

--- manifests/uchiwa.pp
+++ manifests/uchiwa.pp
@@ -1,42 +1,21 @@
 # == Class: role_sensu::uchiwa
 #
-# Based on https://github.com/sensu/sensu-puppet/blob/master/tests/uchiwa.pp
-#
-class role_sensu::uchiwa {
+class role_sensu::uchiwa (
+  $uchiwa_api_config = [{
+                          host     => '127.0.0.1',
+                          ssl      => true,
+                          insecure => true,
+                          port     => 4567,
+                          user     => 'sensu',
+                          pass     => 'sensu',
+                          path     => '/sensu',
+                          timeout  => 5
+                        }]
 
-  package { 'uchiwa':
-    ensure => present,
+  ) {
+
+  class { 'uchiwa':
+    sensu_api_endpoints => $uchiwa_api_config,
   }
 
-  file { '/etc/sensu/uchiwa.json':
-    ensure  => present,
-    content => '
-  {
-    "sensu": [
-      {
-        "name": "Site1",
-        "host": "localhost",
-        "port": 4567,
-        "timeout": 5,
-        "user": "admin",
-        "pass": "secret"
-      }
-    ],
-    "uchiwa": {
-      "host": "0.0.0.0",
-      "port": 3000,
-      "user": "uchiwa",
-      "pass": "uchiwa",
-      "interval": 5
-    }
-  }',
-    require => Package['uchiwa'],
-    notify  => Service['uchiwa'],
-  }
-
-  service { 'uchiwa':
-    ensure  => running,
-    require => [ File['/etc/sensu/uchiwa.json'],Package['uchiwa'] ]
-  }
-  
 }
