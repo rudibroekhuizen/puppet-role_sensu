00aa20d13509cbc0608fd976252e48543ee8a0ad
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-02 11:24:07 +0100

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -4,22 +4,27 @@ class role_sensu::client (
   $rabbitmq_password,
   $sensu_server,
   ) {
+  
+  # Ruby-dev is needed for sensu-plugin
+  package { 'ruby-dev':
+    ensure => installed,
+  } ->
+  
+  # Make is needed for ruby-dev
+  package { 'make':
+    ensure => installed,
+  } ->
 
   class { 'sensu':
     rabbitmq_password    => $rabbitmq_password,
     rabbitmq_host        => $sensu_server,
     rabbitmq_vhost       => '/sensu',
     subscriptions        => 'sensu-test-rudi',
-    sensu_plugin_version => installed,
+    sensu_plugin_version => installed,           #gem install sensu-plugin
     #subscriptions       => 'all',
     #client_address      => $::ipaddress_eth1,
   }
-  
-  #package { 'sensu-plugin':
-  #  ensure   => installed,
-  #  provider => gem,
-  #}
-  
+ 
   vcsrepo { '/opt/sensu-community-plugins':
     ensure   => present,
     provider => git,
