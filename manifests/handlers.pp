11799ea18227b95a73f4e0901be1cfb169d45d22
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-04 11:51:30 +0100

    Create handlers.pp

---
+++ manifests/handlers.pp
@@ -0,0 +1,21 @@
+# == Class: role_sensu::handlers
+#
+class role_sensu::handlers {
+
+  package { 'mail':
+    ensure   => installed,
+    provider => 'gem'
+  }
+
+  sensu::handler { 'mailer':
+    type        => 'pipe',
+    source      => '/opt/sensu-community-plugins/handlers/notification/mailer.rb',
+    config      => {
+      mail_from     => "sensu-rudi@naturalis.nl",
+      mail_to       => "rudi.broekhuizen@naturalis.nl",
+      smtp_address  => "aspmx.l.google.com",
+      smtp_port     => 25,
+      smtp_domain   => "naturalis.nl",
+    },
+  }
+}
