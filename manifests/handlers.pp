349062a81fe6706220bb9e53973be674db73dde9
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-05 11:42:09 +0100

    Update handlers.pp

--- manifests/handlers.pp
+++ manifests/handlers.pp
@@ -13,7 +13,8 @@ class role_sensu::handlers {
 
   sensu::handler { 'mailer':
     type        => 'pipe',
-    command     => '/opt/sensu/embedded/bin/ruby /opt/sensu-community-plugins/handlers/notification/mailer.rb',
+    #command    => '/opt/sensu/embedded/bin/ruby /opt/sensu-community-plugins/handlers/notification/mailer.rb',
+    command     => '/opt/sensu-community-plugins/handlers/notification/mailer.rb',
     config      => {
       mail_from     => "sensu-rudi@naturalis.nl",
       mail_to       => "rudi.broekhuizen@naturalis.nl",
