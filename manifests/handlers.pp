667bef67812a8eac4ec16a0cd6f1379c636ac508
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-05 12:47:50 +0100

    Update handlers.pp

--- manifests/handlers.pp
+++ manifests/handlers.pp
@@ -12,15 +12,14 @@ class role_sensu::handlers {
   }
 
   sensu::handler { 'mailer':
-    type        => 'pipe',
-    #command    => '/opt/sensu/embedded/bin/ruby /opt/sensu-community-plugins/handlers/notification/mailer.rb',
-    command     => '/opt/sensu-community-plugins/handlers/notification/mailer.rb',
-    config      => {
-      mail_from     => "sensu-rudi@naturalis.nl",
-      mail_to       => "rudi.broekhuizen@naturalis.nl",
-      smtp_address  => "aspmx.l.google.com",
-      smtp_port     => 25,
-      smtp_domain   => "naturalis.nl",
+    type    => 'pipe',
+    command => '/opt/sensu/embedded/bin/ruby /opt/sensu-community-plugins/handlers/notification/mailer.rb',
+    config  => {
+      mail_from    => "sensu-rudi@naturalis.nl",
+      mail_to      => "rudi.broekhuizen@naturalis.nl",
+      smtp_address => "aspmx.l.google.com",
+      smtp_port    => 25,
+      smtp_domain  => "naturalis.nl",
     },
   }
 }
