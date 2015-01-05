9825a425447a4bc01ddc58a9b6600d12802d64c4
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-01-05 13:08:05 +0100

    Update handlers.pp

--- manifests/handlers.pp
+++ manifests/handlers.pp
@@ -2,10 +2,11 @@
 #
 class role_sensu::handlers {
 
+  # Needed by mailer handler: /opt/sensu/embedded/bin/gem install mail --no-ri --no-rdoc -v 2.5.4 
   package { mail:
-    ensure          => installed,
+    ensure          => "2.5.4",
     provider        => gem,
-    install_options => [{"--install-dir" => "/opt/sensu/embedded/lib/ruby/gems/2.0.0/gems","-v" => "2.5.4"}],
+    install_options => [{"--install-dir" => "/opt/sensu/embedded/lib/ruby/gems/2.0.0/gems"}],
   } 
 
   sensu::handler { 'default':
