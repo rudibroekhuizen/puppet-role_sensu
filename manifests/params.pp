5581f370c287858ccdcc34e04e617ec5b51030ab
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-06 11:44:49 +0100

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,1 +1,16 @@
-df
+# == Class: role_sensu::params
+#
+class role_sensu::params {
+  $deployment = 'foreman'
+  
+  case $deployment {
+    'foreman': { 
+      $parameters = parseyaml(file('/etc/puppet/hieradata/server-test.yaml')),
+      $configfile = $parameters['role_test::configfile']
+    }
+    'default': {
+      $configfile = 'logstash-snmpget-01.conf'
+    }
+  }
+  
+}
