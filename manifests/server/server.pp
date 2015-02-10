cabb66591bd2910efdb51bf6bd9f9739159c56da
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-10 11:46:44 +0100

    Update server.pp

--- manifests/server/server.pp
+++ manifests/server/server.pp
@@ -1,6 +1,6 @@
-# == Class: role_sensu::server
+# == Class: role_sensu::server::server
 #
-class role_sensu::server (
+class role_sensu::server::server (
   $rabbitmq_password = 'secret',
   $api_user          = 'api_user',
   $api_password      = 'secret',
@@ -8,7 +8,7 @@ class role_sensu::server (
 
   class { 'redis': } ->
 
-  class { 'role_sensu::rabbitmq': } ->
+  class { 'role_sensu::server::rabbitmq': } ->
 
   class { 'sensu':
     install_repo      => true,
@@ -31,11 +31,11 @@ class role_sensu::server (
   }
   
   # Create checks hiera yaml input
-  class { 'role_sensu::checks':
+  class { 'role_sensu::server::checks':
   }
   
   # Create handlers
-  class { 'role_sensu::handlers':
+  class { 'role_sensu::server::handlers':
   }
 
 }
