4cfd5b96653879023354254f776fddbecbd5cb29
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-10 20:27:52 +0100

    Update server.pp

--- manifests/server/server.pp
+++ manifests/server/server.pp
@@ -17,7 +17,7 @@ class role_sensu::server::server (
     server            => true,
     manage_services   => true,
     manage_user       => true,
-    rabbitmq_password => $::rabbitmq_password,
+    rabbitmq_password => $rabbitmq_password,
     rabbitmq_vhost    => '/sensu',
     api               => true,
     api_user          => $api_user,
