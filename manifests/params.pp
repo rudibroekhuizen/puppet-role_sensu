f68f208f319fd21152703bad0c118a7e6bd107d6
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-10 20:34:26 +0100

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,1 +1,5 @@
-$role_sensu::parameters['role_sensu::server::server::rabbitmq_password']
+# == Class: role_sensu::params
+#
+class role_sensu::params {
+  rabbitmq_password = $role_sensu::parameters['role_sensu::server::server::rabbitmq_password']
+}
