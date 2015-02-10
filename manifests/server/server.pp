5c278b95673fb343b099161d287a0b57ed1fde0a
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-10 19:53:43 +0100

    Update server.pp

--- manifests/server/server.pp
+++ manifests/server/server.pp
@@ -1,13 +1,15 @@
 # == Class: role_sensu::server::server
 #
 class role_sensu::server::server (
-  $rabbitmq_password = 'secret',
-  #$rabbitmq_password = $role_sensu::parameters['role_sensu::server::rabbitmq_password'],
+  #$rabbitmq_password = 'secret',
+  $rabbitmq_password = $role_sensu::parameters['role_sensu::server::rabbitmq_password'],
   $api_user          = 'api_user',
   $api_password      = 'secret',
   ) {
+  
   include role_sensu
-  notice( "$role_sensu::parameters['role_sensu::server::rabbitmq_password']" )
+  
+  #notice( "$role_sensu::parameters['role_sensu::server::rabbitmq_password']" )
   
   class { 'redis': } ->
 
