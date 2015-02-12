8cd2c5999c4d01f6cc1169172b65e3d9c1447520
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-12 08:51:51 +0100

    Update server.pp

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,6 +6,8 @@ class role_sensu::server (
   $api_password      = 'secret',
   ) {
   
+  notice( "$role_sensu::parameters['role_sensu::server::rabbitmq_password']" )
+  
   class { 'redis': } ->
 
   class { 'role_sensu::rabbitmq': } ->
