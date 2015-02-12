a81caff061a3b05d0d023bcf6fadb7ce2ffe05d3
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-12 13:40:05 +0100

    Update dashboard.pp

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -2,8 +2,8 @@
 #
 class role_sensu::dashboard (
   $uchiwa_api_config = $role_sensu::yaml::parameters['role_sensu::dashboard::uchiwa_api_config'],
-  $uchiwa_user       = 'admin',
-  $uchiwa_pass       = 'secret',
+  $uchiwa_user       = $role_sensu::yaml::parameters['role_sensu::dashboard::uchiwa_user'],
+  $uchiwa_pass       = $role_sensu::yaml::parameters['role_sensu::dashboard::uchiwa_pass'],
   ) {
 
   # Install uchiwa, username and password for uchiwa webinterface, port 3000.
