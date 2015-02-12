fe2639ed1b5e3d47fcc941acc2b44450845ccda5
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-02-12 09:11:54 +0100

    Update checks.pp

--- manifests/checks.pp
+++ manifests/checks.pp
@@ -1,8 +1,8 @@
 # == Class: role_sensu::checks
 #
 class role_sensu::checks (
-  $checks_hash = $role_sensu::parameters['role_sensu::checks::checks_hash'],
-  $checks_defaults = $role_sensu::parameters['role_sensu::checks::checks_defaults'],
+  $checks_hash = $role_sensu::yaml::parameters['role_sensu::checks::checks_hash'],
+  $checks_defaults = $role_sensu::yaml::parameters['role_sensu::checks::checks_defaults'],
   ){
 
   create_resources('sensu::check', $checks_hash, $checks_defaults)
