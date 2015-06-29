73f02eab264b24737ba2e466447044c507c24037
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2015-06-29 15:36:23 +0200

    Update dashboard.pp

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -1,16 +1,12 @@
 # == Class: role_sensu::dashboard
 #
-class role_sensu::dashboard (
-  $uchiwa_api_config = $role_sensu::yaml::parameters['role_sensu::dashboard::uchiwa_api_config'],
-  $uchiwa_user       = $role_sensu::yaml::parameters['role_sensu::dashboard::uchiwa_user'],
-  $uchiwa_pass       = $role_sensu::yaml::parameters['role_sensu::dashboard::uchiwa_pass'],
-  ) {
+class role_sensu::dashboard {
 
   # Install uchiwa, username and password for uchiwa webinterface, port 3000.
   class { 'uchiwa':
-    sensu_api_endpoints => $uchiwa_api_config,
-    user                => $uchiwa_user,
-    pass                => $uchiwa_pass,
+    sensu_api_endpoints => $role_sensu::uchiwa_api_config,
+    user                => $role_sensu::uchiwa_user,
+    pass                => $role_sensu::uchiwa_pass,
     install_repo        => false            # otherwise you get this: Apt::Source[sensu] is already declared in file /etc/puppet/modules/sensu/manifests/repo/apt.pp
   }
 
