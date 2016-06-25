cc1bd8255086d0a8627f50e30e220fac05927a33
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-06-25 11:35:54 +0200

    Update and rename dashboard.pp to uchiwa.pp

--- manifests/uchiwa.pp
+++ manifests/uchiwa.pp
@@ -1,9 +1,9 @@
-# == Class: role_sensu::dashboard
+# == Class: role_sensu::uchiwa
 #
-class role_sensu::dashboard {
+class role_sensu::uchiwa {
 
   # Install uchiwa, username and password for uchiwa webinterface, port 3000.
-  class { 'uchiwa':
+  class { '::uchiwa':
     sensu_api_endpoints => $role_sensu::uchiwa_api_config,
     user                => $role_sensu::uchiwa_user,
     pass                => $role_sensu::uchiwa_pass,
@@ -11,7 +11,7 @@ class role_sensu::dashboard {
   }
 
   # If using uchiwa ssl settings: "bind: permission denied"; using nginx
-  class {'nginx': }
+  class {'::nginx': }
 
   nginx::resource::upstream { 'sensu':
     members => ['localhost:3000'],
