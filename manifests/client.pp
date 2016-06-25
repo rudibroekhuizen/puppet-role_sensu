a2373e2fa047a771799d9c653b086b35fc35f2c0
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-06-25 12:14:00 +0200

    Update client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -2,6 +2,20 @@
 #
 class role_sensu::client {
   
+  # Create private key file for rabbitmq communication between client and server:
+  file { '/etc/sensu/ssl/private_key.pem':
+    ensure  => present,
+    content => $role_sensu::private_key,
+    mode    => '0644',
+  }
+  
+  # Create cert chain file:
+  file { '/etc/sensu/ssl/cert_chain.pem':
+    ensure  => present,
+    content => $role_sensu::cert_chain,
+    mode    => '0644',
+  }
+
   class { 'sensu':
     rabbitmq_vhost    => '/sensu',
     rabbitmq_password => $role_sensu::rabbitmq_password,
