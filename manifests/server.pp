7799424e31b4734a00ae208078c47d04370415e5
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-06-27 14:15:58 +0200

    SSL support

--- manifests/server.pp
+++ manifests/server.pp
@@ -6,17 +6,20 @@ class role_sensu::server {
 
   class { 'role_sensu::rabbitmq': } ->
 
+  # Use Sensu SSL tool to generate certificates
   class { 'sensu':
-    install_repo      => true,
-    server            => true,
-    manage_services   => true,
-    manage_user       => true,
-    rabbitmq_password => $role_sensu::rabbitmq_password,
-    rabbitmq_vhost    => '/sensu',
-    api               => true,
-    api_user          => $role_sensu::api_user,
-    api_password      => $role_sensu::api_password,
-    use_embedded_ruby => true,  # /opt/sensu/embedded/bin/ruby 
+    install_repo             => true,
+    server                   => true,
+    manage_services          => true,
+    manage_user              => true,
+    rabbitmq_password        => $role_sensu::rabbitmq_password,
+    rabbitmq_ssl_cert_chain  => '/etc/sensu/ssl/cert.pem',
+    rabbitmq_ssl_private_key => '/etc/sensu/ssl/key.pem',
+    rabbitmq_vhost           => '/sensu',
+    api                      => true,
+    api_user                 => $role_sensu::api_user,
+    api_password             => $role_sensu::api_password,
+    use_embedded_ruby        => true,  # /opt/sensu/embedded/bin/ruby 
   }
   
   # Create checks hiera yaml input
