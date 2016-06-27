a7e29116736a0bc923d7d35d20f2ec2163af7190
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2016-06-27 12:53:33 +0200

    SSL support

--- manifests/rabbitmq.pp
+++ manifests/rabbitmq.pp
@@ -3,9 +3,13 @@
 class role_sensu::rabbitmq {
 
   class {'::rabbitmq':
+    ssl_key           => '/etc/rabbitmq/ssl/key.pem',
+    ssl_cert          => '/etc/rabbitmq/ssl/cert.pem',
+    ssl_cacert        => '/etc/rabbitmq/ssl/cacert.pem',
+    ssl               => true,
     delete_guest_user => true,
   }
- 
+
   rabbitmq_vhost { '/sensu': }
 
   rabbitmq_user { 'sensu': 
